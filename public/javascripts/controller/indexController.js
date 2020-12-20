5d34a5071bd49f594ec73e2a5d8511444edbbeef
MertYldrmm <mert.yyldrim@gmail.com>
2020-12-21 00:31:23 +0300

    disconnect event

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,36 +1,53 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
-    $scope.messages = [ ];
-    $scope.init = () => {
-        const username = prompt('Please enter username');
+  $scope.messages = [ ];
+  $scope.init = () => {
+    const username = prompt('Please enter username');
 
-        if(username)
-            initSocket(username);
-        else
-            return false;
+    if(username)
+      initSocket(username);
+    else
+      return false;
+  };
+
+  function initSocket(username){
+    const connectionOptions = {
+      reconnectionAttempts: 3,
+        reconnectionDelay: 600
     };
+   
+    indexFactory.connectSocket('http://localhost:3000', connectionOptions)
+      .then((socket) => {
+        socket.emit('new user', {username});
+               
+        socket.on('new user', (data) => {
+          const messageData = {
+            type: { // info
+              code: 0,  //server or user message
+              message: 1  // login or disconnect message
+            }, 
+            username: data.username
+          };
 
-    function initSocket(username){
-        const connectionOptions = {
-            reconnectionAttempts: 3,
-            reconnectionDelay: 600
-        };
-    
-        indexFactory.connectSocket('http://localhost:3000', connectionOptions)
-            .then((socket) => {
-                socket.emit('new user', {username});
-                
-                socket.on('new user', (data) => {
-                    const messageData = {
-                        type: 0, // info
-                        username: data.username
-                    };
-
-                    $scope.messages.push(messageData);
-                    $scope.$apply();
-                });
-            }).catch((err) => {
-                console.log(err);
+          $scope.messages.push(messageData);
+          $scope.$apply();
         });
-    }
+
+        socket.on('disUser', (data) => {
+          const messageData = {
+            type: { // info
+              code: 0,  //server or user message
+              message: 0  // login or disconnect message
+            }, 
+            username: data.username
+          };
+
+          $scope.messages.push(messageData);
+          $scope.$apply();
+        });
+
+      }).catch((err) => {
+        console.log(err);
+      });
+  }
 
 }]);
--- src/socketApi.js
+++ src/socketApi.js
@@ -4,25 +4,32 @@ const io = socketio();
 const socketApi = { };
 socketApi.io=io;
 
-const users = [ ];
+const users = { };
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+  console.log('a user connected');
 
-    socket.on('new user', (data) => {
-        const defaultData = {
-            id: socket.id,
-            position:{
-                x: 0,
-                y: 0
-            }
-        };
+  socket.on('new user', (data) => {
+    const defaultData = {
+      id: socket.id,
+        position:{
+          x: 0,
+          y: 0
+        }
+    };
 
-        const userData = Object.assign(data, defaultData);
-        users.push(userData);
+    const userData = Object.assign(data, defaultData);
+    users[socket.id] = userData;
+    console.log(users);
+    socket.broadcast.emit('new user', users[socket.id]);
+  });
+  
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('disUser', users[socket.id]);  
+    delete users[socket.id];
+    console.log(users);
+  });
 
-        socket.broadcast.emit('new user', userData);
-    });
 });
 
 module.exports = socketApi;
